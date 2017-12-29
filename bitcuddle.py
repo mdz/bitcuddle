d4ceb4e5fa034bc1ed8acdddc6f0a3ee7a2f8e97
Matt Zimmerman <mdz+github@alcor.net>
2017-12-28 18:36:40 -0800

    Automatically connect lnd to a peer at startup

--- bitcuddle.py
+++ bitcuddle.py
@@ -16,3 +16,22 @@ stub = lnrpc.LightningStub(channel)
 
 response = stub.GetInfo(ln.GetInfoRequest())
 print(response)
+
+lnd_key = os.environ['LND_PEER_PUBKEY']
+lnd_host = os.environ['LND_PEER_HOST']
+lnd_address = ln.LightningAddress(pubkey=lnd_key, host=lnd_host)
+
+response = stub.ListPeers(ln.ListPeersRequest())
+print(repr(response))
+
+peered = False
+for peer in response.peers:
+    if peer.pub_key == lnd_key:
+        peered = True
+
+if peered:
+    print("Already peered with {}".format(lnd_key))
+else:
+    print("Peering with {}@{}".format(lnd_key,lnd_host))
+    response = stub.ConnectPeer(ln.ConnectPeerRequest(addr=lnd_address, perm=True))
+    print(response)
